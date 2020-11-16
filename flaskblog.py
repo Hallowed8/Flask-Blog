81861bab317f8ebf1b887b43b0d82ea181e2ea05
Adrian Aleksiak <adrianaleksiak@gmail.com>
2020-11-16 19:11:52 +0100

    Added login and register templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'f62efed8759d359688119ad62affe698'
 
 posts = [
     {
@@ -28,6 +30,15 @@ def hello():
 def about():
     return render_template("about.html", title = 'About')
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Register', form = form)
 
 if __name__  == '__main__':
     app.run(debug = True)
