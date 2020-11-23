756a29b3a90c53aa99316c443b6cc3e5ae03e1e5
Adrian Aleksiak <adrianaleksiak@gmail.com>
2020-11-23 23:45:44 +0100

    added user authentication and basic account page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog.models import User, Post
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog import app, db, bcrypt
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -31,6 +31,8 @@ def about():
 
 @app.route("/register", methods = ['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -43,14 +45,28 @@ def register():
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         if form.validate_on_submit():
             user = User.query.filter_by(email = form.email.data).first()
             if user and bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
+                flash("You're now logged in.", 'success')
                 return redirect(url_for('home'))
             else:
                 flash('Unsuccessful login. Please check username and password', 'danger')
 
     return render_template('login.html', title = 'Register', form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("You've been logged out", 'info')
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title = 'Account')
