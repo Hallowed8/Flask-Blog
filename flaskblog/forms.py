70d860802932faad88357aadc0f237e3c34877d5
Adrian Aleksiak <adrianaleksiak@gmail.com>
2020-11-25 03:04:49 +0100

    added change profile picture option into account form

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min=2, max = 25)])
@@ -42,6 +43,7 @@ class UpdateAccountForm(FlaskForm):
     email = StringField('Email', validators = [DataRequired(), Email()])
     username = StringField('Username', validators = [DataRequired(), Length(min=2, max = 25)])
     submit = SubmitField('Update')
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
 
     def validate_username(self, username):
         if username.data != current_user.username:
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,6 +3,9 @@ from flaskblog.models import User, Post
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
+from PIL import Image
 
 posts = [
     {
@@ -66,11 +69,28 @@ def logout():
     flash("You've been logged out", 'info')
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex+f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnall(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
+
 @app.route('/account', methods = ['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
