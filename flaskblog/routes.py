52d47e96035c5c61cfe950a9a4c44fd41c988855
Adrian Aleksiak <adrianaleksiak@gmail.com>
2020-11-26 14:16:00 +0100

    added route and page for a specific post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -99,3 +99,11 @@ def new_post():
         flash('Your post has been created', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form= form, legend='New Post')
+
+@app.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title = post.title, post=post)
+
+
+    
