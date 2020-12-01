41b619ef5b2c2ac79216f8852d45027116a60092
Adrian Aleksiak <adrianaleksiak@gmail.com>
2020-12-01 04:21:13 +0100

    Oredered posts by posted date; added page with user-specific posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -181,3 +181,12 @@ def reset_token(token):
         db.session.commit()
         flash(f'You password has been updated! You are now able to log in', category='success')
         return redirect(url_for('login'))
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type = int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author = user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page = 5)
+    return render_template("user_posts.html", posts = posts, user=user)
